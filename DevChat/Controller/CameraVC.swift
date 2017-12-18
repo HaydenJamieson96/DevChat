508f0d19dfc3d960b13fdb86bb86d38278f4f6ad
HaydenJamieson96 <32482976+HaydenJamieson96@users.noreply.github.com>
2017-12-18 14:31:54 +0000

    Initial commit

---
+++ DevChat/Controller/CameraVC.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  DevChat
+//
+//  Created by TheAppExperts on 12/18/17.
+//  Copyright © 2017 TheAppExperts. All rights reserved.
+//
+
+import UIKit
+
+class CameraVC: AAPLCameraViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+
+
+}
+
